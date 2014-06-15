4c4a36bc111a56a548b118dbb02ef45173496561
Victor Neo <icyisamu@gmail.com>
2014-06-15 23:36:36 +0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clog "0.1.0-SNAPSHOT"
   :description "Clog is blogging web app built in Clojure"
   :url "http://github.com/victorneo/clog"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.1"]
-                 [selmer "0.5.9"]
-                 [org.clojure/java.jdbc "0.3.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"]
+                 [selmer "0.6.6"]
+                 [org.clojure/java.jdbc "0.3.3"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [enlive "1.1.5"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.8"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:init clog.db/create-db
          :handler clog.routes/app}
